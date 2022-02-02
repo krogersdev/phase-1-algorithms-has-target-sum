d9397715318fabf3e66befbc85c220a71a8d3c16
Kevin Rogers <kevinarogers1@gmail.com>
2022-02-02 17:55:31 -0500

    commit tree 6b24e2fd249c691e66d0b66ef49b2a290e0e585b

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for ( let i = 0; i < array.length; i++ ) {
+    //runtime: n steps 
+    for ( let k = i + 1; k < array.length; k++ ) {
+    // //runtime: 0(n * n ) steps 
+        if( array[k] + array[i] === target ) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime : 0(n^2)
+  space : 0(n)
+
+
 */
 
 /* 
   Add your pseudocode here
+  for an input array and a target number i.e [3, 8, 12, 4, 11, 7], 10)
+   iterate through the array once to get our current number and iterate a second time while checling whether: current number + proceeding number = target
+   return true if we find a match, otherwise return false. 
+  
 */
 
 /*
   Add written explanation of your solution here
+  I am using a nested for..Loop. First loop iterates the array from index 0 selecting each number, the nested/second loop starts at index 1 in order to create a sum of the current and nested iteration. 
+  i used a If statement to check whether the sum of the first and nested iteration equals our target number. If we find a match we return true, else false. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +46,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1], 4));
 }
 
 module.exports = hasTargetSum;
